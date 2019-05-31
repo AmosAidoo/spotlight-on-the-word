be0ab16e42b436cf8e97c87b618592994bc83e8c
AmosAidoo <aidooamos20@gmail.com>
2019-05-31 08:26:14 +0000

    Added the team cards to the about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,23 +20,63 @@
         <!--Our Team-->
         <div>
             <h1 class="display-4 text-center">Our Team</h1>
-
             <div class="row">
-                <div class="col-md-3">
-                    <h3>Member 1</h3>
+                <div class="col-md-6 d-flex justify-content-center">
+                        <div class="card" style="width: 20rem;">
+                            <img src="{{ asset('aged.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                
+                            </div>
+                        </div>
                 </div>
 
-                <div class="col-md-3">
-                    <h3>Member 2</h3>
+                <div class="col-md-6 d-flex justify-content-center">
+                        <div class="card" style="width: 20rem;">
+                            <img src="{{ asset('aged.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('aged.jpg')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
                 </div>
 
-                <div class="col-md-3">
-                    <h3>Member 3</h3>
+                <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="{{ asset('aged.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                
+                            </div>
+                        </div>
                 </div>
 
-                <div class="col-md-3">
-                    <h3>Member 4</h3>
+                <div class="col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="{{ asset('aged.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                
+                            </div>
+                        </div>
                 </div>
+
             </div>
         </div>
         
