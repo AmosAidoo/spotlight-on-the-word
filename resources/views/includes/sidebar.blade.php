d7eeb6fe98540c0066ac7470a4cc10273d9e5cb6
AmosAidoo <aidooamos20@gmail.com>
2019-06-01 14:41:35 +0000

    I've added sidebar to the includes directory

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,39 @@
+<div>
+    <div class="left-bar-item">
+        <h3 class="sidebar-title mb-2">Youtube</h3>
+        <iframe style="width:100%; height:200px" src="https://www.youtube.com/embed/1Q7XR_1EZRI" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+    <div class="br"></div>
+
+    <div class="left-bar-item">
+        <h3 class="sidebar-title">Social Media</h3>
+    </div>
+
+    <div class="br"></div>
+
+    <div class="left-bar-item">
+        <div class="latest-article">
+            <h3 class="sidebar-title">Latest Articles</h3>
+            <div class="media post_item">
+                <img class="img-fluid" src="{{ asset('aged.jpg')}}" width="100" height="60"/>
+                <div class="media-body ml-3">
+                    <a href="blog-details.html"><h3 style="font-size: 18px">Space The Final Frontier</h3></a>
+                <p>02 Hours ago</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="left-bar-item">
+        <h3 class="sidebar-title">Tags</h3>
+
+        <a href="#" class="tag-link"><span class="tag">Salvation</span></a>
+        <a href="#" class="tag-link"><span class="tag">Faith</span></a>
+        <a href="#" class="tag-link"><span class="tag">Love</span></a>
+        <a href="#" class="tag-link"><span class="tag">Hope</span></a>
+        <a href="#" class="tag-link"><span class="tag">Sin</span></a>
+    </div>
+
+</div>
