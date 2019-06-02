505b5d1b060444d49c5051f1d876c056b8b3bbbc
AmosAidoo <aidooamos20@gmail.com>
2019-06-02 17:30:16 +0000

    This is the migration for the categories table

---
+++ database/migrations/2019_06_01_151532_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropUnique('categories_name_unique');
+        });
+
+        Schema::dropIfExists('categories');
+    }
+}
