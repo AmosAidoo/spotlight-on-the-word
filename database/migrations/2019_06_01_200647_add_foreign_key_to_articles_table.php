e00261b7848ced2b9d2d3f6cb31910ab32286935
AmosAidoo <aidooamos20@gmail.com>
2019-06-02 17:31:19 +0000

    This migration references category in the article relation to name in the categories relation

---
+++ database/migrations/2019_06_01_200647_add_foreign_key_to_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyToArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->foreign('category')->references('name')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign('articles_category_foreign');
+        });
+    }
+}
