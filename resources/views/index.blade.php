2df5f4759933784a6d77b233d78491ea5a70ea50
AmosAidoo <aidooamos20@gmail.com>
2019-06-07 12:00:30 +0000

    Removed the owl carousel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,35 +13,9 @@
         <div class="row">
             <!--Articles Highlights-->
             <div class="col-lg-8 mt-2">
-                <h3 class="p-2 pl-0 mb-4" style="border-left:solid 3px #111E6C">Events</h3>
-                {{-- <div class="mb-4">
-                    <img class="img-fluid" src="{{ asset('aged.jpg')}}" alt="">
-                </div> --}}
-                <div class="owl-carousel owl-theme mb-3 w-100">
-                        <div class="owl-inner">
-                                <div id="sotv" class="item p-4 text-center" style="background-image: url({{ asset('bible1.jpg')}});">
-                                    <h1 class="display-4">SPOTLIGHT ON THE VERSE</h1>
-                                    <p>
-                                        God is spirit and they that worship Him
-                                        must worship Him in spirit and in truth
-
-                                        <p style="text-align:right">John 4:24</p>
-                                    </p>
-                                    {{-- <img class="img-fluid" src="{{ asset('aged.jpg')}}"/> --}}
-                                </div>
-                        </div>
-                    
-                        <div class="owl-inner">
-                                <div class="item" style="background-image: url({{ asset('bible2.jpg')}});">
-                                        <h1 class="text-center
-                                        ">IN SEARCH OF THE SCRIPTURES</h1>
-                                </div>
-                        </div>
-                        <div class="owl-inner">
-                                <div class="item" style="background-image: url({{ asset('aged.jpg')}});">
-                                    {{-- <img class="img-fluid" src="{{ asset('aged.jpg')}}"/> --}}
-                                </div>
-                        </div>
+                <h3 class="p-2 pl-0 mb-4" style="border-left:solid 3px #111E6C">Upcoming Event</h3>
+                <div class=" mb-4">
+                    <img class="img-fluid" src="{{ asset('aged.jpg')}}" alt="#">
                 </div>
                 
                 <h3 class="p-2 pl-0 mb-4" style="border-left:solid 3px #2866c9">Articles</h3>
@@ -77,7 +51,7 @@
             </div>
 
             <!--Right side bar-->
-            <div id="sidebar" class="col-lg-4 mt-2 p-5">
+            <div class="sidebar col-lg-4 mt-2 p-5">
                 @include('includes.sidebar')
             </div>
         </div>
