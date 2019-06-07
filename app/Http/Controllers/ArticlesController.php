642c35189fde59d921a8d465885d0825c82a04dc
AmosAidoo <aidooamos20@gmail.com>
2019-06-07 12:01:36 +0000

    I've added a postByTag function

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -17,12 +17,16 @@ class ArticlesController extends Controller
                             ->select('id', 'title', 'category', 'image', 'body', 'created_at', 'author')
                             ->where('category', $category)
                             ->orderBy('id', 'desc')
-                            ->get();
+                            ->paginate(10);
         
         $date = DB::table('articles')
                 ->select(DB::raw('date(created_at) as date'))
                 ->get();
         return view('res.resource', ['posts' => $posts, 'category' => $category, 'latest' => $latest]);
+    }
+
+    public function postsByTag($tag){
+        
     }
 
     public function showPost($category, $id){
